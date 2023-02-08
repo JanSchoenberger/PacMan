a6b706a9cef09109510a185237b8ee8c018143a2
JanSchoenberger <janschoenberger@outlook.de>
2023-02-08 16:07:09 +0100

    Version 08.02.23

---
+++ Pacman.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Pacman:
+    
+	sprite = None
+	bewegungsRichtung = None
+	
+	def __init__ (self):
+		self.bewegungsRichtung = "links"
+
+
+"""
+Pacman
+	Sprite
+	Bewegungsrichtung
+	Startpunkt
+	Rectangle
+"""
