1dfa3e9e1d9b6f4a3e350c76a78cf977252b6205
JanSchoenberger <janschoenberger@outlook.de>
2023-02-09 09:44:36 +0100

    Aktuelle Version 09.02.23

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame.event
+from pygame import *
+import Feld
+import Wand
+import Spielfeld
+import os
+import csv
+
+def importCSV():
+    with open("assets/LVL.CSV") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=";")
+        temArray = [[]]*22
+        lineCounter = 0
+        for row in csv_reader:
+            temArray[lineCounter]=row
+            lineCounter += 1
+            #print(lineCounter)
+        return temArray
+
+def main():
+    levelStrArr = importCSV()
+
+    spielfeld = Spielfeld.Spielfeld(levelStrArr)
+    fensterbreite = 608
+    fensterhöhe = 704
+    fenster = pygame.display.set_mode((fensterbreite, fensterhöhe))
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        clock.tick(60)
+        spielfeld.drawSpielfeld(fenster)
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                running = False
+            if event.type == KEYDOWN and event.key == K_d:
+                print("d gedrückt") 
+                
+
+
+if __name__ == '__main__':
+    main()
+# 16, 9
