c0048d274461e7ce1b89cff480d1579f11665895
Maxxyi <124058686+Maxxyi@users.noreply.github.com>
2023-02-14 15:57:05 +0100

    Add files via upload
    kleine file Schnipsel

---
+++ testo.py
@@ -0,0 +1,6 @@
+fenster.blit(Pacman.bildPacMan, (270,480),(bildbereich[frame])
+                pygame.display.update()
+                time.sleep(0.2)
+#spielerfigur = Pacmanbild
+bildformat = (700, 500)
+spielerfigur = pygame.transform.scale(spielerfigur, bildformat)
